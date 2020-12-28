e5df6129637399da12d4cddc401fa76ba4a231f7
newaz_subra <newaz.sharif@subrasystems.com>
2020-12-29 00:58:00 +0600

    first commit

---
+++ PCOHRApp/Scripts/Application_Scripts/AllInternetCustomerDetails.js
@@ -0,0 +1,58 @@
+﻿
+$(document).ready(function () {
+    loadInitialization();
+});
+
+$('#btnSaveId').click(function () {
+    GenerateRDLC('PDF');
+});
+
+$('#btnExcelId').click(function () {
+    GenerateRDLC('Excel');
+});
+$('#btnWordId').click(function () {
+    GenerateRDLC('Word');
+});
+
+
+function GenerateRDLC(fileType) {
+    var activeStatus = $('#activeStatusId').val() == "" ? null : $('#activeStatusId').val() == '1' ? true : false;
+    var zoneId = $('#zoneId').val();
+    var reportType = 'AllInternetCustomerDetails';
+    var viewURL = '/CableReport/ShowReport?fileType=' + fileType + '&isActive=' + activeStatus + '&zoneId=' + zoneId + "&reportType=" + reportType;
+    $.fancybox(
+		{
+		    'title': 'Report Window',
+		    'type': 'iframe',
+		    'transitionIn': 'elastic',
+		    'transitionOut': 'elastic',
+		    'speedIn': 1000,
+		    'speedOut': 700,
+		    autoSize: false,
+		    closeClick: false,
+		    afterShow: after_show(fileType),
+		    'href': viewURL
+		});
+}
+function after_show(fileType) {
+    if (fileType != "PDF") {
+        setTimeout(function () { $.fancybox.close(); }, 19000);
+    }
+}
+
+
+
+
+function loadInitialization() {
+    $('#messageBoxId').hide();
+    $('#errorMessageBoxId').hide();
+    $('#successMessageBoxId').hide();
+
+
+    $.get('/Zone/GetZoneListForDropdown', function (data) {
+        zones = data.data;
+        $('#zoneId').select2({
+            data: zones
+        });
+    });
+}
---
+++ PCOHRApp/Scripts/Application_Scripts/DishCustomerDue.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+    loadInitialization();
+});
+
+$('#btnSaveId').click(function () {
+    GenerateRDLC("PDF");
+});
+$('#btnExcelId').click(function () {
+    GenerateRDLC('Excel');
+});
+$('#btnWordId').click(function () {
+    GenerateRDLC('Word');
+});
+
+function loadInitialization() {
+    $('#messageBoxId').hide();
+    $('#errorMessageBoxId').hide();
+    $('#successMessageBoxId').hide();
+
+
+    $.get('/Zone/GetZoneListForDropdown', function (data) {
+        zones = data.data;
+        $('#zoneId').select2({
+            data: zones
+        });
+    });
+}
+
+
+function GenerateRDLC(fileType) {
+    var zoneId = $('#zoneId').val();
+    var reportType = 'DishBillDue';
+    var viewURL = '/CableReport/ShowReport?fileType=' + fileType + '&zoneId=' + zoneId + "&reportType=" + reportType;
+    $.fancybox(
+        {
+            'title': 'Report Window',
+            'type': 'iframe',
+            'transitionIn': 'elastic',
+            'transitionOut': 'elastic',
+            'speedIn': 1000,
+            'speedOut': 700,
+            autoSize: false,
+            closeClick: false,
+            afterShow: after_show(fileType),
+            'href': viewURL
+        });
+}
+function after_show(fileType) {
+    if (fileType != "PDF") {
+        setTimeout(function () { $.fancybox.close(); }, 19000);
+    }
+}
